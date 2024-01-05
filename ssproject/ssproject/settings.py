b7bee338fc9d14c76c7739d3ae142df677918862
Nikita Profir <neicker44536@gmail.com>
2024-01-05 15:05:26 +0300

    deploy v1

--- ssproject/ssproject/settings.py
+++ ssproject/ssproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1wu1=$#*k36$7ph-*^qoti&4fg3x%kcn_d6bigk$opbbrqz@_1'
+SECRET_KEY = getenv("SECRET_KEY")
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
